70b724f4dbf0eb9e091604b726e3c43501aaf0ee
Ajay Chavan <ajayc9838@gmail.com>
2023-03-27 10:54:48 +0530

    created domian class user and address and added lombok dependency in userAuth pom

---
+++ UserAuth/src/main/java/com/niit/UserAuth/domain/Address.java
@@ -0,0 +1,10 @@
+package com.niit.UserAuth.domain;
+
+public class Address {
+    private String buildingName;
+    private String streetName;
+    private int flatNo;
+    private String city;
+    private String state;
+    private double pincode;
+}
--- UserAuth/src/main/java/com/niit/UserAuth/domain/User.java
+++ UserAuth/src/main/java/com/niit/UserAuth/domain/User.java
@@ -1,4 +1,15 @@
 package com.niit.UserAuth.domain;
 
+import java.util.List;
+
 public class User {
+
+    private String email;
+    private String firstName;
+    private String lastname;
+    private long mobileNo;
+    private String password;
+    private String role;
+    private String status;
+    private List<Address> address;
 }
