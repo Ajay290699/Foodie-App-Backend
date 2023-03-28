5515d06b4f4901016b3e276a8581108d23d76a6f
Ajay Chavan <ajayc9838@gmail.com>
2023-03-28 14:35:49 +0530

    created a routing method which have a common url

--- ApiGateWay/src/main/java/com/niit/ApiGateWay/configuration/AppConfiguration.java
+++ ApiGateWay/src/main/java/com/niit/ApiGateWay/configuration/AppConfiguration.java
@@ -1,4 +1,17 @@
 package com.niit.ApiGateWay.configuration;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfiguration {
+
+    @Bean
+    public RouteLocator getRoutes(RouteLocatorBuilder builder) {
+        return builder.routes().route(p -> p.path
+                ("/app/v1/**").uri("lb://UserAuth")).route(p -> p.path
+                ("/app/v2/**").uri("lb: //Restaurant-Service")).build();
+    }
 }
