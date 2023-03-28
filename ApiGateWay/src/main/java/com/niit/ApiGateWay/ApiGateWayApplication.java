94a44932110c282fd78c700cbf51bce2dc4ee64d
Ajay Chavan <ajayc9838@gmail.com>
2023-03-28 14:39:35 +0530

    added eurekaclient dependeny in all service's

--- ApiGateWay/src/main/java/com/niit/ApiGateWay/ApiGateWayApplication.java
+++ ApiGateWay/src/main/java/com/niit/ApiGateWay/ApiGateWayApplication.java
@@ -2,8 +2,10 @@ package com.niit.ApiGateWay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGateWayApplication {
 
     public static void main(String[] args) {
