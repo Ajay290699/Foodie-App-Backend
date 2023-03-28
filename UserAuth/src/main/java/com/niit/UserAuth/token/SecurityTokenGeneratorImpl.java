bb192b65b998e28859c9769ef55e789865cca833
Ajay Chavan <ajayc9838@gmail.com>
2023-03-28 11:01:39 +0530

    updated token generator method

--- UserAuth/src/main/java/com/niit/UserAuth/token/SecurityTokenGeneratorImpl.java
+++ UserAuth/src/main/java/com/niit/UserAuth/token/SecurityTokenGeneratorImpl.java
@@ -14,12 +14,32 @@ public class SecurityTokenGeneratorImpl implements SecurityTokenGenerator {
 
     @Override
     public Map<String, String> tokenGenerator(User user) {
-        String jwtToken = null;
-        jwtToken = Jwts.builder().setSubject(user.getEmail()).setIssuedAt(new Date()).
-                signWith(SignatureAlgorithm.HS256, "securityKey").compact();
-        Map<String, String> map = new HashMap<>();
-        map.put("token", jwtToken);
-        map.put("message", "User Logged in successfully");
-        return map;
+
+        //rajeshwar sir method
+
+        Map<String, String> token = new HashMap<>();
+        Map<String, Object> tokenData = new HashMap<>();
+        tokenData.put("emailId", user.getEmail());
+        tokenData.put("password", user.getPassword());
+
+        String jwtToken = Jwts.builder()
+                .setClaims(tokenData)
+                .setIssuedAt(new Date())
+                .setIssuer("FoodieApp_user")
+                .signWith(SignatureAlgorithm.HS512, "securityKey")
+                .compact();
+
+        token.put("token", jwtToken);
+        token.put("message", "login successfull, token generated");
+        return token;
+
+//        gayatri ma'am method
+//        String jwtToken = null;
+//        jwtToken = Jwts.builder().setSubject(user.getEmail()).setIssuedAt(new Date()).
+//                signWith(SignatureAlgorithm.HS256, "securityKey").compact();
+//        Map<String, String> map = new HashMap<>();
+//        map.put("token", jwtToken);
+//        map.put("message", "User Logged in successfully");
+//        return map;
     }
 }
