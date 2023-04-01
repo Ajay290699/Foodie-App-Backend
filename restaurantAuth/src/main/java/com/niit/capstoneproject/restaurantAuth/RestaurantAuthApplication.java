9940e288f0e074da17f3dedae8f00dd82b5329b0
Ajay Chavan <ajayc9838@gmail.com>
2023-04-01 17:57:28 +0530

    restaurant-auth runned successfully

--- restaurantAuth/src/main/java/com/niit/capstoneproject/restaurantAuth/RestaurantAuthApplication.java
+++ restaurantAuth/src/main/java/com/niit/capstoneproject/restaurantAuth/RestaurantAuthApplication.java
@@ -4,11 +4,13 @@ import com.niit.capstoneproject.restaurantAuth.services.RestaurantAuthService;
 import com.niit.capstoneproject.restaurantAuth.services.RestaurantServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication  //(scanBasePackages = {"controller","service","models", "repos"})
 public class RestaurantAuthApplication {
 
