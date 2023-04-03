00b4df813c747bb5f1655abbd411804ebe7f8947
Ajay Chavan <ajayc9838@gmail.com>
2023-04-03 09:45:28 +0530

    changed dependency

--- restaurant-service/src/main/java/com/niit/stackroute/restaurant/service/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/niit/stackroute/restaurant/service/RestaurantServiceApplication.java
@@ -6,12 +6,14 @@ import com.niit.stackroute.restaurant.service.service.RestaurantService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+@EnableEurekaClient
 @SpringBootApplication  //(scanBasePackages = {"controller","service","models", "repos"})
 public class RestaurantServiceApplication {
 
