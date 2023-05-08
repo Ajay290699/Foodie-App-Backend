27850ceda9899c1f82d9d8c54ef072e266d08e80
Ajay Chavan <ajayc9838@gmail.com>
2023-05-08 09:53:16 +0530

    Restaurant Service test cases added

--- RestaurantService/src/test/java/com/niit/RestaurantService/service/RestaurantServiceTest.java
+++ RestaurantService/src/test/java/com/niit/RestaurantService/service/RestaurantServiceTest.java
@@ -3,6 +3,7 @@ package com.niit.RestaurantService.service;
 import com.niit.RestaurantService.models.Dishes;
 import com.niit.RestaurantService.models.Restaurant;
 import com.niit.RestaurantService.models.RestaurantOwner;
+import com.niit.RestaurantService.repos.DishesRepo;
 import com.niit.RestaurantService.repos.OwnerRepo;
 import com.niit.RestaurantService.repos.RestaurantRepo;
 import org.junit.jupiter.api.AfterEach;
@@ -13,9 +14,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class RestaurantServiceTest {
@@ -27,14 +32,17 @@ public class RestaurantServiceTest {
     @Mock
     private OwnerRepo ownerRepo;
 
+    @Mock
+    private DishesRepo dishesRepo;
+
     @InjectMocks
     private RestaurantImpl restaurantService;
 
     private Dishes dishes;
 
-    private List<Restaurant> restaurantList;
+    private Restaurant restaurant1;
 
-    private Restaurant restaurant;
+    private List<Restaurant> restaurant;
 
     private RestaurantOwner restaurantOwner;
 
@@ -42,12 +50,13 @@ public class RestaurantServiceTest {
 
     @BeforeEach
     void setUp() {
-//        Dishes disheSet = new HashSet<Dishes>();
+        dishesSet = new ArrayList<Dishes>();
         dishes = new Dishes("pizza", "veg", 45);
         dishesSet.add(dishes);
-        restaurant = new Restaurant("XYZ Hotel", "mumbai", dishesSet);
-        restaurantList.add(restaurant);
-        restaurantOwner = new RestaurantOwner("abc@gmail.com", "ABC", restaurantList);
+        restaurant = new ArrayList<>();
+        restaurant1 = new Restaurant("XYZ Hotel", "mumbai", dishesSet);
+        restaurant.add(restaurant1);
+        restaurantOwner = new RestaurantOwner("abc@gmail.com", "ABC", restaurant);
     }
 
     @AfterEach
@@ -60,18 +69,44 @@ public class RestaurantServiceTest {
 
     @Test
     void addRestaurantOwner() {
-//        RestaurantOwner restaurantOwner1 = new RestaurantOwner();
-//        restaurantOwner1.setEmailId("res@gmail.com");
-//        restaurantOwner1.setOwnerName("restaurant");
-//        restaurantOwner1.setRestaurant(restaurant);
-        when(restaurantRepo.save(restaurantOwner)).thenReturn(restaurantOwner);
+        when(ownerRepo.save(restaurantOwner)).thenReturn(restaurantOwner);
         RestaurantOwner savedOwner = restaurantService.addOwner(restaurantOwner);
         assertEquals(restaurantOwner, savedOwner);
     }
 
     @Test
     void addRestaurant() {
-        when(restaurantRepo.save(restaurant)).thenReturn(restaurant);
-        assertEquals();
+        when(ownerRepo.findById(restaurantOwner.getEmailId())).thenReturn(Optional.ofNullable(restaurantOwner));
+        when(restaurantRepo.save(restaurant1)).thenReturn(restaurant1);
+        when(ownerRepo.save(restaurantOwner)).thenReturn(restaurantOwner);
+        assertEquals(restaurantOwner, restaurantService.addRestaurant(restaurantOwner.getEmailId(), restaurant1));
+    }
+
+    @Test
+    void addRestaurantFail() {
+        when(ownerRepo.findById(restaurantOwner.getEmailId())).thenReturn(Optional.ofNullable(restaurantOwner));
+        when(restaurantRepo.save(restaurant1)).thenReturn(restaurant1);
+        when(ownerRepo.save(restaurantOwner)).thenReturn(null);
+        assertNotEquals(restaurantOwner, restaurantService.addRestaurant(restaurantOwner.getEmailId(), restaurant1));
+    }
+
+    @Test
+    void addDish() {
+        when(restaurantRepo.findById(restaurant1.getRestaurantName())).thenReturn(Optional.ofNullable(restaurant1));
+        when(dishesRepo.save(dishes)).thenReturn(dishes);
+        restaurant1.addDishes(dishes);
+        when(restaurantRepo.save(restaurant1)).thenReturn(restaurant1);
+
+        assertEquals(restaurant1, restaurantService.addDishesToRestaurant(restaurant1.getRestaurantName(), dishes));
+    }
+
+    @Test
+    void addDishFail() {
+        when(restaurantRepo.findById(restaurant1.getRestaurantName())).thenReturn(Optional.ofNullable(restaurant1));
+        when(dishesRepo.save(dishes)).thenReturn(dishes);
+        restaurant1.addDishes(dishes);
+        when(restaurantRepo.save(restaurant1)).thenReturn(restaurant1);
+
+        assertNotEquals(null, restaurantService.addDishesToRestaurant(restaurant1.getRestaurantName(), dishes));
     }
 }
